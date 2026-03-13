f8a6920d389a154cdc7c20664bf803c0bc943266
anixcn <anixcnu@gmail.com>
2026-03-13 09:26:48 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 int main()
 {
+    printf("% 10s", "name");
+    printf("% 1s", "id");
+    printf("% 1s\n", "value");
 
+    printf("% 10s", "kim");
+    printf("% 1d", 12);
+    printf("% 1f\n", 4.12);
 
-    return 0;
+    printf("% 10s", "lee");
+    printf("% 1d", 1922);
+    printf("% 1f\n", 3.21);
+
+    printf("% 10s", "park");
+    printf("% 1d", 432);
+    printf("% 1f\n", 1.0);
+
+        return 0;
 }
