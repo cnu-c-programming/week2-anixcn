9db45c81fb92ab4bf211b3341d8095803e8addec
anixcn <anixcnu@gmail.com>
2026-03-13 10:25:02 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,30 @@
 
 int main()
 {
+	int num1;
+	int num2;
+	char c;
+
+	scanf("%d %d %c", &num1, &num2, &c);
+
+
+	switch (c) {
+	case '+':
+		printf("%d", (num1+ num2));
+		break;
+	case '-':
+		printf("%d", (num1- num2));
+		break;
+	case '*':
+		printf("%d", (num1* num2));
+		break;
+	case '/':
+		printf("%d", (num1/num2));
+		break;
+
+		default: 
+			break; 
+	}
 
     return 0;
 }
