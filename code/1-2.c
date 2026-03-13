f4de045223eb3622b7d5a39fbcf54ea8c794f1d3
anixcn <anixcnu@gmail.com>
2026-03-13 09:47:36 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    printf("% 10s", "name");
-    printf("% 11s", "id");
-    printf("% 10s\n", "value");
-
-    printf("% 10s","kim");
-    printf("% 1s%", "");
-    printf("%010d", 12); 
-    printf("% 10.2f\n", 4.12);
-
-    printf("% 10s", "lee");
-    printf("% 1s%", "");
-    printf("%010d", 1922);
-    printf("% 10.2f\n", 3.21);
-
-    printf("% 10s", "park");
-    printf("% 1s%", "");
-    printf("%010d", 432);
-    printf("% 10.2f\n", 1.0);
+    printf("%10s %10s %10s\n", "name", "id", "value");
+    printf("%10s %010d % 10.2f\n", "kim", 12, 4.12);
+    printf("%10s %010d % 10.2f\n", "lee", 1922, 3.21);
+    printf("%10s %010d % 10.2f\n", "park", 432, 1.0);
 
     return 0;
 }
