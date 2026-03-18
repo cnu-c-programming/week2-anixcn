54bb8d2cd5e6dd2fe2bbe902f115aad2dcf4efae
anixcn <anixcnu@gmail.com>
2026-03-18 22:00:44 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,24 @@
 
 int main()
 {
+	int num = 0;
+	scanf("%d", &num);
 
-    return 0;
+	int arr[num+1];
+
+	for (int i = 0; i <= num; i++) {
+		arr[i] = i;
+	}
+
+	for (int i = 2; i < num; i++) {
+		if (arr[i] == 0) continue;
+		for (int j = i * i; j <= num; j += i) {
+			arr[j] = 0;
+		}
+	}
+
+	if (arr[num] != 0) { printf("%s", "true") ;}
+	if (arr[num] == 0) {printf("%s", "false");}
+
+return 0;
 }
-
